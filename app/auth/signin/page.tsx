8c0d8adef3517d9ef09b1408ba8fea8c559a51f1
Razboth <razboth14@gmail.com>
2025-12-14 00:13:59 +0800

    revert: Remove red logo above login and forgot password forms

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -146,15 +146,6 @@ function SignInForm() {
             />
           </div>
 
-          {/* Logo above form */}
-          <div className="hidden lg:block">
-            <img
-              src="/logo-bsg.png"
-              alt="Bank SulutGo"
-              className="h-14 w-auto"
-            />
-          </div>
-
           {/* Header */}
           <div className="text-center lg:text-left">
             <h2 className="text-2xl font-bold tracking-tight text-foreground">
